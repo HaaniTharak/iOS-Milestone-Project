0358b709e0f9b2de64304f8f57bca40c235eed65
Haani Tharak <haani@pal-nat186-55-228.itap.purdue.edu>
2022-10-24 20:28:37 -0400

    App Uploaded

---
+++ Second Sexiest App/Second Sexiest App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Second Sexiest App
+//
+//  Created by Haani Tharak on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
